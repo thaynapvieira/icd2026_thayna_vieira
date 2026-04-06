3c82046d5f06f5b74f27f6b9c7a74ee9f2745d08
Thayná vieira <thaynavieira2012@gmail.com>
2026-04-06 20:17:42 -0300

    Alterado lista 02(inicio)

---
+++ lista-exercicios/02-lista.R
@@ -0,0 +1,80 @@
+# Arquivo: 02-lista.R
+# Autor(a): Thayna Pereira Vieira
+# Data: 06/04/2026
+# Objetivo:
+# 1. Resolver os exercícios da lista 2
+
+
+# Configurações globais ---------------------------------------------------
+
+# Configura o número de dígitos a serem exibidos
+options(digits = 5, scipen = 999)
+
+# carrega os pacotes necessários
+
+# 1. Carregar os pacotes necessários
+library(here)
+library(tidyverse) # Nota: O pacote readr é carregado automaticamente com o tidyverse
+library(janitor)
+
+# 2. Importar o arquivo dados-marketing.csv da pasta dados/brutos
+dados_marketing <- read_csv(here("dados", "brutos", "dados-marketing.csv"))
+
+# 3. Executar o glimpse para analisar a estrutura dos dados
+glimpse(dados_marketing)
+
+# Exercicio 1 -------------------------------------------------------------
+
+
+## a)
+
+
+## b)
+
+
+## c)
+
+
+
+# Exercicio 2 -------------------------------------------------------------
+
+## a)
+
+
+## b)
+
+
+# Exercicio 3 -------------------------------------------------------------
+
+## a)
+
+
+## b)
+
+
+
+# Exercicio 4 -------------------------------------------------------------
+
+
+
+# Exercicio 5 -------------------------------------------------------------
+
+
+
+# Exercicio 6 -------------------------------------------------------------
+
+
+
+
+# Exercicio 7 -------------------------------------------------------------
+
+
+
+
+# Exercicio 8 -------------------------------------------------------------
+
+
+
+
+
+# Exercicio 9 -------------------------------------------------------------
